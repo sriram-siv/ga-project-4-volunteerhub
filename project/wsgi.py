4e40fb4247e4aa913e401b565831f9cacc2be1cd
Sriram Sivarajah <sriramsivarajah@gmail.com>
2020-11-20 16:19:33 +0000

    point gunicorn at routing

--- project/wsgi.py
+++ project/wsgi.py
@@ -10,21 +10,7 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-import chat_messages.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
-# application = get_wsgi_application()
-
-application = ProtocolTypeRouter({
-    # (http->django views is added by default)
-    'http': get_wsgi_application(),
-    'https': get_wsgi_application(),
-    'websocket': AuthMiddlewareStack(
-        URLRouter(
-            chat_messages.routing.websocket_urlpatterns
-        )
-    ),
-})
+application = get_wsgi_application()
