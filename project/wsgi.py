33927436164b1ddb9caeaa77de7687d8121d71de
Sriram Sivarajah <sriramsivarajah@gmail.com>
2020-11-20 16:13:13 +0000

    revert wsgi file

--- project/wsgi.py
+++ project/wsgi.py
@@ -11,20 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-import chat_messages.routing
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = get_wsgi_application()
-
-# application = ProtocolTypeRouter({
-#     "http": get_wsgi_application(),
-#     # (http->django views is added by default)
-#     'websocket': AuthMiddlewareStack(
-#         URLRouter(
-#             chat_messages.routing.websocket_urlpatterns
-#         )
-#     ),
-# })
