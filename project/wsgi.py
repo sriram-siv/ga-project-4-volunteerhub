e779722b11206aac341a2f0693cba01d27e587bd
Sriram Sivarajah <sriramsivarajah@gmail.com>
2020-11-20 15:42:58 +0000

    fixed imports for protocol router

--- project/wsgi.py
+++ project/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import chat_messages.routing
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 # application = get_wsgi_application()
