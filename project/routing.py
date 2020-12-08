58ade6b11edc8733cb94ac96ba9b35c654ff0214
Sriram Sivarajah <sriramsivarajah@gmail.com>
2020-12-08 12:02:59 +0000

    initialise settings module in routing.py

--- project/routing.py
+++ project/routing.py
@@ -2,6 +2,10 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import chat_messages.routing
 
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
     'websocket': AuthMiddlewareStack(
