ac53903ca2e787e8ed3b6b6491730a142ff0c123
Sriram Sivarajah <sriramsivarajah@gmail.com>
2020-12-08 11:45:47 +0000

    changed project settings to ref routing.py as the asgi app

--- project/settings.py
+++ project/settings.py
@@ -86,7 +86,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'project.wsgi.application'
-ASGI_APPLICATION = 'project.asgi.application'
+ASGI_APPLICATION = 'project.routing.application'
 
 CHANNEL_LAYERS = {}
 if str(os.getenv('ENVIRONMENT')) == 'development':
