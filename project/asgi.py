46537ff4b8848d93403c638021498b5f10ef8afe
Sriram Sivarajah <sriramsivarajah@gmail.com>
2020-12-08 13:16:46 +0000

    calling setup from asgi.py

--- project/asgi.py
+++ project/asgi.py
@@ -8,10 +8,13 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
 import os
+import django
+from channels.routing import get_default_application
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+django.setup()
 
 # from django.core.asgi import get_asgi_application
-from channels.routing import get_default_application
 
 # application = get_asgi_application()
 application = get_default_application()
