4391f5f198f7ef8bc1f860c3f68bc9c417b68c69
Sriram Sivarajah <sriramsivarajah@gmail.com>
2020-12-08 13:12:22 +0000

    changed app to use channels method

--- project/asgi.py
+++ project/asgi.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
-from django.core.asgi import get_asgi_application
+# from django.core.asgi import get_asgi_application
+from channels.routing import get_default_application
 
-application = get_asgi_application()
+# application = get_asgi_application()
+application = get_default_application()
